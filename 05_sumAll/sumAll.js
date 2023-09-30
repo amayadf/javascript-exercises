ca6be6a29736a30cb0c64b79221b4ba2951c9ddf
Amaya Delgado Fuentes <amayadfuentes@gmail.com>
2023-09-30 16:46:55 -0400

    Finish exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if(number1 < 0 || number2 < 0 || !Number.isInteger(number1) || !Number.isInteger(number2)) return 'ERROR';
+    
+    let biggerNumber = (number1 > number2) ? number1 : number2;
+    let smallerNumber = (number1 < number2) ? number1 : number2;
 
+    let sum = 0;
+    for(let i = smallerNumber; i < biggerNumber+1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
