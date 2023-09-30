ced16e10fb0c813d5167a18ef96d0384c90014b3
Amaya Delgado Fuentes <amayadfuentes@gmail.com>
2023-09-30 15:59:33 -0400

    Finish exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return "ERROR";
+    let modifiedString = "";
+    for(let i = 0; i < num; i++) {
+        modifiedString += string;
+    }
+    return modifiedString;
 };
 
 // Do not edit below this line
